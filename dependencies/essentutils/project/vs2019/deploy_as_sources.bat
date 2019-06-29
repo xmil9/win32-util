4b144f4bbf2624e2b97c2be46fba550a786cc399
Mike Lindner <mlindner9@yahoo.com>
2019-06-29 18:23:13 -0400

    Deployment script for sources.

---
+++ dependencies/essentutils/project/vs2019/deploy_as_sources.bat
@@ -0,0 +1,25 @@
+::
+:: Deploys project and sources of the essentutils library to a given
+:: directory. Will create a subdirectory for the library.
+:: Example: > .\deploy_as_sources.bat C:\libs
+::
+@echo off
+
+set outDir=%1
+set thisDir=%~dp0
+
+set src=%thisDir%..\..
+set dest=%outDir%\essentutils
+
+:: Deploy code.
+mkdir %dest%
+robocopy %src% %dest% *.h
+robocopy %src% %dest% *.cpp
+
+:: Deploy project.
+set from=%src%\project\vs2019
+set to=%dest%\project\vs2019
+mkdir %to%
+robocopy %from% %to% essentutils.vcxproj
+robocopy %from% %to% essentutils.vcxproj.filters
+robocopy %from% %to% deploy_as_sources.bat
---
+++ project/deploy_as_sources.bat
@@ -0,0 +1,27 @@
+::
+:: Deploys project and sources of the win32-util library to a given
+:: directory. Will create a subdirectory for the library.
+:: Example: > .\deploy_as_sources.bat C:\libs
+::
+@echo off
+
+set outDir=%1
+set thisDir=%~dp0
+
+set src=%thisDir%..
+set dest=%outDir%\win32_util
+
+:: Deploy code.
+mkdir %dest%
+robocopy %src% %dest% *.h
+robocopy %src% %dest% *.cpp
+
+:: Deploy project.
+set from=%src%\project
+set to=%dest%\project
+mkdir %to%
+robocopy %from% %to% win32_util.vcxproj
+robocopy %from% %to% win32_util.vcxproj.filters
+
+:: Deploy dependencies by calling deployment scripts of each dependency.
+call %src%\dependencies\essentutils\project\vs2019\deploy_as_sources.bat %dest%\dependencies
