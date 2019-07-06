dbf0f9ae275eeb024193d618b8efaee70b0ba572
Mike Lindner <mlindner9@yahoo.com>
2019-07-06 11:07:17 -0400

    Update dependencies.

--- dependencies/essentutils/color.h
+++ dependencies/essentutils/color.h
@@ -7,6 +7,8 @@
 //
 #pragma once
 #include <cstdint>
+#include <initializer_list>
+#include <utility>
 
 
 namespace sutil
@@ -22,6 +24,8 @@ struct Rgb
    Rgb& operator=(const Rgb&) = default;
    Rgb& operator=(Rgb&&) = default;
 
+   friend void swap(Rgb& a, Rgb& b) noexcept;
+
    std::uint8_t r = 0;
    std::uint8_t g = 0;
    std::uint8_t b = 0;
@@ -30,6 +34,26 @@ struct Rgb
 
 inline Rgb::Rgb(std::uint8_t r_, std::uint8_t g_, std::uint8_t b_) : r{r_}, g{g_}, b{b_}
 {
+}
+
+inline void swap(Rgb& a, Rgb& b) noexcept
+{
+   std::swap(a.r, b.r);
+   std::swap(a.g, b.g);
+   std::swap(a.b, b.b);
+}
+
+
+// Comparision
+
+inline bool operator==(const Rgb& a, const Rgb& b)
+{
+   return (a.r == b.r && a.g == b.g && a.b == b.b);
+}
+
+inline bool operator!=(const Rgb& a, const Rgb& b)
+{
+   return !(a == b);
 }
 
 } // namespace util
